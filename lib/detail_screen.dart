471a0b18d2d8d358f989cf213668b002ecd72fc8
malininds <MalininDS70@yandex.ru>
2024-02-20 14:29:22 +0700

    Небольшие изменения 19.02.24

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -10,83 +10,96 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white12,
+      appBar: AppBar(
+        backgroundColor: Colors.white12,
+        iconTheme: const IconThemeData(
+          color: Colors.yellow,
+          size: 30,
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(data.image),
-              const SizedBox(height: 50),
-              Text(
-                data.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 15),
+                Image.network(data.image),
+                const SizedBox(height: 15),
+                Text(
+                  data.title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                data.subTitle,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
+                const SizedBox(height: 15),
+                Text(
+                  data.subTitle,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                data.date,
-                style: const TextStyle(
-                  color: Colors.cyan,
-                  fontSize: 15,
+                const SizedBox(height: 15),
+                Text(
+                  data.date,
+                  style: const TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                'Center:',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.6),
-                  fontSize: 18,
+                const SizedBox(height: 15),
+                Text(
+                  'Center:',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.6),
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              Text(
-                data.center,
-                style: const TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 15,
+                Text(
+                  data.center,
+                  style: const TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                'Key words:',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.6),
-                  fontSize: 18,
+                const SizedBox(height: 15),
+                Text(
+                  'Key words:',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.6),
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              Text(
-                data.keyWord!.join((", ")),
-                style: const TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 15,
+                Text(
+                  data.keyWord!.join((", ")),
+                  style: const TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                'NASA ID: ',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.6),
-                  fontSize: 18,
+                const SizedBox(height: 15),
+                Text(
+                  'NASA ID: ',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.6),
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              Text(
-                data.id,
-                style: const TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 15,
+                Text(
+                  data.id,
+                  style: const TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
           ),
         ),
       ),
